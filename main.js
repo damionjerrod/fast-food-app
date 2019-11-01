8f3d4192c21135cc255a415b5cc11460fa6adee0
damionjerrod <damion.dawson@gmail.com>
2019-11-01 15:57:03 -0400

    what I have so far...

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 var menu = {
     "hamburger": 50,
     "cheesburger": 50,
-    "Fries": 50
+    "fries": 50,
+    "drinks": 50
 }
 
 var customerOrder = undefined;
 var order = [];
 
-//gets data from input field and prints it
+//gets data from input field and prints it on the screen
 document.getElementById("submit").addEventListener("click", function() {
     customerOrder = document.getElementById("entree").value;
     document.getElementById("result").innerHTML = customerOrder;
     console.log(customerOrder);
-    customerOrder = customerOrder.split(',').map(s => s.trim());
-    //used .map to loop through in order to trim whitespace
-    
-    order.push(customerOrder);
+
+    //converts order to string, clears whitespace, and separates it into an array
+    customerOrder = customerOrder.toString().split(',').map(s => s.trim());
+    order = customerOrder;
     console.log(order);
 
+    //loops through array, separates item from amt, & clears whitespace
     for (i = 0; i < order.length; i++) {
-        console.log(order[i]);
-        for (j = 0; j < order[i].length; j++) {
-            console.log(order[i][j]);
-        }
+        order[i] = order[i].split(':').map(s => s.trim());
+        console.log(order[i]);      
     }
 
-    
-});
+    for (item in menu) {
+        if (menu.hasOwnProperty(order[0][1])) {
+            console.log("on menu");
+        }
+    }
+})
 
 //clears input field after submission
 document.getElementById("submit").addEventListener("click", function() {
     document.getElementById("entree").value = '';
 })
 
+        //for (j = 0; j < order[i].length; j++) {
+        //    console.log(order[i][j]);
+        //}
