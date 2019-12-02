391e3d363d187791c8554c832404390d8a217e8f
damionjerrod <damion.dawson@gmail.com>
2019-12-02 14:14:39 -0500

    starting database

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 let http = require("http");
-let url = require("url");
+let path = require("path");
 let fs = require("fs");
-const port = 8080;
+let port = 8080;
 
 let server = http.createServer(function(req, res) {
-    fs.readFile("./index.html", function(error, html) {  
+    let filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+    
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, html) {  
         if (error) {  
             res.writeHead(404);  
             res.write(error);  
             res.end();  
         } else {  
             res.writeHead(200, {  
-                'Content-Type': 'text/html'  
+                'Content-Type': contentType  
             });  
             res.write(html);  
             res.end();  
