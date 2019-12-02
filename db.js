c81047a7762eaa12d95359930eed088ac1383e9b
damionjerrod <damion.dawson@gmail.com>
2019-12-02 15:33:50 -0500

    fastfood app with working database

---
+++ db.js
@@ -0,0 +1,49 @@
+let mysql = require('mysql');
+
+let db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "mydawson84",
+    database: "fast_food",
+    multipleStatements: true
+});
+
+db.connect((err) =>{
+    if(!err){
+        console.log("Connected to Database");
+    } else {
+        console.log("Connection Failed");
+        console.error(err);
+    }
+});
+
+function getAllInventory(){
+    db.query('SELECT * FROM inventory', function(err, rows, fields){
+        if(!err)
+        console.log(rows);
+        else
+        console.log(err);
+    })
+};
+
+function setNewItem(itemName, itemCount){
+    let itemInsert = `INSERT INTO inventory (item_name, item_count) VALUES ("${itemName}", ${itemCount})`;
+    db.query(itemInsert, function(err, result){
+        if(!err){
+        console.log(result);
+        console.log("Items Successfully Added to Database");
+        } else {
+        console.log(err);
+        }
+    })
+};
+
+
+// console.log(setNewItem("Hamburger", 2));
+console.log(getAllInventory());
+
+module.exports = getAllInventory();
+module.exports = setNewItem(itemName, itemCount){
+    this.itemName = itemName;
+    this.itemCount = itemCount;
+};
