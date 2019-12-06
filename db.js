adb7ad928296ae7b14d3cf452a684bc7bd303e0a
damionjerrod <damion.dawson@gmail.com>
2019-12-05 19:17:16 -0500

    endpoint routes serving up json data on dom

--- db.js
+++ db.js
@@ -1,4 +1,23 @@
+const mysql = require('mysql');
 const fs = require('fs');
+
+//database connection:
+let db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "mydawson84",
+    database: "fast_food",
+    multipleStatements: true
+});
+
+db.connect((err) => {
+    if(!err){
+        console.log("Connected to database");
+    } else {
+        console.log("Connection failed");
+        console.log(err);
+    }
+});
 
 module.exports = {
     getAllInventory: (req, res) => {
@@ -12,6 +31,8 @@ module.exports = {
         });
     },
     setNewItem: (req, res) => {
+        let itemName = req.body.name;
+        let itemCount = req.body.count;
         let itemInsert = `INSERT INTO inventory (item_name, item_count) VALUES ("${itemName}", ${itemCount})`;
         db.query(itemInsert, (err, result) => {
             if(!err) {
--- server.js
+++ server.js
@@ -1,33 +1,10 @@
-const mysql = require('mysql');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const fileUpload = require('express-fileupload');
 const express = require('express');
 const app = express();
 const port = 8080;
 
-//my modules:
-const {getAllInventory, setNewItem} = require('./liveInventory.js');
-
-
-//database connection:
-let db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mydawson84",
-    database: "fast_food",
-    multipleStatements: true
-});
-
-db.connect((err) => {
-    if(!err){
-        console.log("Connected to database");
-    } else {
-        console.log("Connection failed");
-        console.log(err);
-    }
-});
-
+const {getAllInventory, setNewItem} = require('./db.js');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,17 +14,6 @@ app.get('/api/show', getAllInventory);
 app.get('/index', function(req, res){
     res.send('you are requesting the index page');
 });
-
-// app.get('/api/show', function(req, res){
-//     console.log(useDb.getAllInventory());
-//     // res.send(currentInventory);
-//     // console.log(typeof(currentInventory));
-// });
-
-// app.post('/api/add', function(req, res){
-    
-// });
-
 
 app.listen(port, function(){
     console.log(`Server listening on port: ${port}`);
